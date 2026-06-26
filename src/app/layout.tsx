8f8f08065edffc3e8ddc16fd7b80d058714241f4
sifatbhatia <sifatbhatia@gmail.com>
2026-06-26 03:27:48 -0700

    glimm: lagoon palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
       className={`${sans.variable} ${mono.variable} ${display.variable} h-full antialiased`}
     >
       <body className="h-full bg-bg text-text font-sans">
-          <GlimmProvider palette="ember" sweepMs={1200} easing="easeOutCubic">
+          <GlimmProvider palette="lagoon" sweepMs={1200} easing="easeOutCubic">
             <InterceptLinks />
         <TRPCProvider>
           <Providers>
